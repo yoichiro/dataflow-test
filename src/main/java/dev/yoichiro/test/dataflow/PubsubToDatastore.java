d5b02925d9c969f13eee37de0a43b494316b7191
Yoichiro Tanaka <yoichiro6642@gmail.com>
2019-08-14 17:10:00 +0900

    Support event time.

--- src/main/java/dev/yoichiro/test/dataflow/PubsubToDatastore.java
+++ src/main/java/dev/yoichiro/test/dataflow/PubsubToDatastore.java
@@ -6,7 +6,10 @@ import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.Sum;
+import org.apache.beam.sdk.transforms.windowing.AfterWatermark;
 import org.apache.beam.sdk.transforms.windowing.FixedWindows;
+import org.apache.beam.sdk.transforms.windowing.Repeatedly;
+import org.apache.beam.sdk.transforms.windowing.TimestampCombiner;
 import org.apache.beam.sdk.transforms.windowing.Window;
 import org.joda.time.Duration;
 
@@ -26,11 +29,12 @@ public class PubsubToDatastore {
         pipeline.apply("read from Pubsub 2", PubsubIO.readStrings().fromTopic(options.getFromTopic()))
                 .apply("simple output data 2", ParDo.of(new SimpleOutputDataFn<String>()));
 
-        pipeline.apply("read from Pubsub 3", PubsubIO.readStrings().fromTopic(options.getFromTopic()))
-//                .apply(Window.<String>into(FixedWindows.of(Duration.standardMinutes(1)))
-//                        .withAllowedLateness(Duration.standardMinutes(5))
-//                        .discardingFiredPanes())
-                .apply(Window.<String>into(FixedWindows.of(Duration.standardMinutes(1))))
+        pipeline.apply("read from Pubsub 3", PubsubIO.readStrings().fromTopic(options.getFromTopic())
+                        .withTimestampAttribute("eventTimestamp"))
+                .apply(Window.<String>into(FixedWindows.of(Duration.standardMinutes(1)))
+                        .triggering(Repeatedly.forever(AfterWatermark.pastEndOfWindow()))
+                        .withAllowedLateness(Duration.standardMinutes(10))
+                        .accumulatingFiredPanes())
                 .apply(ParDo.of(new ExtractWordCountFn()))
                 .apply(Sum.integersPerKey())
                 .apply("simple output data 3", ParDo.of(new SimpleOutputDataFn<>()));
