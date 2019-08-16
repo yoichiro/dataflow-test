b0fd3b1b691097d98cc6a4bb51bd7303d96129ca
Yoichiro Tanaka <yoichiro6642@gmail.com>
2019-08-16 10:25:41 +0900

    Support Daily Window.

--- src/main/java/dev/yoichiro/test/dataflow/PubsubToDatastore.java
+++ src/main/java/dev/yoichiro/test/dataflow/PubsubToDatastore.java
@@ -5,12 +5,9 @@ import org.apache.beam.sdk.io.gcp.datastore.DatastoreIO;
 import org.apache.beam.sdk.io.gcp.pubsub.PubsubIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.*;
-import org.apache.beam.sdk.transforms.windowing.AfterWatermark;
-import org.apache.beam.sdk.transforms.windowing.FixedWindows;
-import org.apache.beam.sdk.transforms.windowing.Repeatedly;
-import org.apache.beam.sdk.transforms.windowing.TimestampCombiner;
-import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.transforms.windowing.*;
 import org.apache.beam.sdk.values.KV;
+import org.joda.time.DateTimeZone;
 import org.joda.time.Duration;
 
 public class PubsubToDatastore {
@@ -53,6 +50,31 @@ public class PubsubToDatastore {
                 .apply(Distinct.create())
                 .apply(Combine.globally(Count.<String>combineFn()).withoutDefaults())
                 .apply("simple output data 4", ParDo.of(new SimpleOutputDataFn<>()));
+
+        // Pubsub -> Window -> Sum -> Stdout
+        pipeline.apply("read from Pubsub 5", PubsubIO.readStrings().fromTopic(options.getFromTopic())
+                        .withTimestampAttribute("eventTimestamp"))
+                .apply(Window.<String>into(
+                            FixedWindows.of(Duration.standardDays(1))
+                                    .withOffset(Duration.standardHours(15)))
+                        .triggering(Repeatedly.forever(AfterPane.elementCountAtLeast(1)))
+                        .withAllowedLateness(Duration.standardMinutes(10))
+                        .accumulatingFiredPanes())
+                .apply(ParDo.of(new ExtractWordCountFn()))
+                .apply(Sum.integersPerKey())
+                .apply("simple output data 3", ParDo.of(new SimpleOutputDataFn<>()));
+
+        // Pubsub -> Window -> Sum -> Stdout
+        pipeline.apply("read from Pubsub 6", PubsubIO.readStrings().fromTopic(options.getFromTopic())
+                        .withTimestampAttribute("eventTimestamp"))
+                .apply(Window.<String>into(
+                        CalendarWindows.days(1).withTimeZone(DateTimeZone.forID("Asia/Tokyo")))
+                        .triggering(Repeatedly.forever(AfterPane.elementCountAtLeast(1)))
+                        .withAllowedLateness(Duration.standardMinutes(10))
+                        .accumulatingFiredPanes())
+                .apply(ParDo.of(new ExtractWordCountFn()))
+                .apply(Sum.integersPerKey())
+                .apply("simple output data 6", ParDo.of(new SimpleOutputDataFn<>()));
 
         pipeline.run().waitUntilFinish();
     }
--- src/main/java/dev/yoichiro/test/dataflow/SimpleOutputDataFn.java
+++ src/main/java/dev/yoichiro/test/dataflow/SimpleOutputDataFn.java
@@ -2,6 +2,8 @@ package dev.yoichiro.test.dataflow;
 
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.windowing.BoundedWindow;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,11 +12,13 @@ public class SimpleOutputDataFn<T> extends DoFn<T, String> {
 
     private static final Logger LOG = LoggerFactory.getLogger(SimpleOutputDataFn.class);
 
+    private static DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss.SSS ZZZ");
+
     @ProcessElement
     public void processElement(ProcessContext c, BoundedWindow window) {
         T input = c.element();
         LOG.info("Window: " + window.toString());
-        LOG.info("Window.maxTimestamp: " + window.maxTimestamp().toString());
+        LOG.info("Window.maxTimestamp: " + formatter.print(window.maxTimestamp()));
         LOG.info("Input: " + input.toString());
         c.output(input.toString());
     }
